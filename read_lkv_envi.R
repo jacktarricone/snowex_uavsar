2167fd66c76cea8c5c6e15da080ff449dec078aa
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2021-07-09 17:43:09 -0700

    start of axis shifting

--- read_lkv_envi.R
+++ read_lkv_envi.R
@@ -4,6 +4,11 @@ library(terra)
 library(rgdal)
 library(data.table)
 library(caTools)
+library(sf)
+library(ggmap)
+library(maps)
+library(tmap)
+
 
 ##### look vector
 
@@ -52,11 +57,49 @@ write.ENVI(lat, "/Volumes/JT/projects/uavsar/jemez/look_vector/BU/envi_files/lat
 write.ENVI(lon, "/Volumes/JT/projects/uavsar/jemez/look_vector/BU/envi_files/lon.llh", interleave = c("bsq"))
 write.ENVI(ele, "/Volumes/JT/projects/uavsar/jemez/look_vector/BU/envi_files/ele.llh", interleave = c("bsq"))
 
-up_tiff <- rast("/Volumes/JT/projects/uavsar/jemez/look_vector/BU/envi_files/up.lkv.tiff")
-up_tiff
-insar_dem <-rast("/Volumes/JT/projects/uavsar/jemez/raw_data/02122020_02192020/DEM/alamos_35915_20005-003_20008-000_0007d_s01_L090HH_01.hgt.grd.tiff")
-insar_dem
-ele_resamp <-resample(up_tiff, insar_dem, method = "bilinear")
+#############################
+#### rotation of axes
+###########################
+
+#### lat lon
+# Corner 1 (&) = 35.734333038, -106.314704895
+# Corner 2 (&) = 35.718479156, -106.553138733       
+# Corner 3 (&) = 36.107688904, -106.334304810    
+# Corner 4 (&) = 36.091930389, -106.563705444  
+
+#### utm zone 13 north (easting, northing)
+# Corner 1 (&) = 381109, 3955279 
+# Corner 2 (&) = 359518, 3953836
+# Corner 3 (&) = 379903, 3996717
+# Corner 4 (&) = 359225, 3995277
+
+
+#nm
+nm_shape <-readOGR("/Volumes/JT/projects/uavsar/jemez/look_vector/nm_shape/tl_2017_35_tract.shp") 
+
+# create pts df
+pts<-as.matrix(rbind(c(381109, 3955279 ), c(359518, 3953836), c(379903, 3996717), c(359225, 3995277)))
+names(pts) <-c('x','y')
+corners <-st_multipoint(pts)
+plot(corners, main = "Corners")
+class(corners)
+
+#convert to corrdinates and define initial coordinates systems
+coordinates(corners) <- c('x', 'y')
+proj4string(corners)=CRS("+init=epsg:32613")  #32613 is the grid for new mexico
 plot(ele_resamp)
-ele_resamp
-plot(insar_dem)
+plot(corners)
+
+#Transform the Ireland's grid to longitude & latitude
+coord<-spTransform(point,CRS("+init=epsg:4326"))
+coord
+
+?tm_shape
+tm_shape(nm_shape) +
+  tm_polygons() +
+  tm_graticules(col = "grey60") +
+  tm_shape(corners) +
+  tm_symbols(col = "black") +
+  tm_scale_bar(position = c("left", "bottom")) +
+  tm_shape(pts) +
+  tm_text("pt", ymod = -1)
