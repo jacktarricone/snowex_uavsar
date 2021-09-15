0a4ea4c1f6aa31aa10da3f40568c17f6f0b2655c
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2021-09-15 16:33:29 -0700

    breaking the lkv and llh data into single bands to be converted to vrts

--- read_lkv_envi.R
+++ read_lkv_envi.R
@@ -1,6 +1,9 @@
 # attempt envi reader in r
+# updated sept 15th
 
-library(terra)
+# trying to read in binary llh file, split it into lat, lon, elevation
+# and then resave single layers back into binary format so can be read into python properly
+
 library(rgdal)
 library(data.table)
 library(caTools)
@@ -8,98 +11,153 @@ library(sf)
 library(ggmap)
 library(maps)
 library(tmap)
+library(terra)
+
+# import the llh file using read.ENVI, no geo data attached just envi formatting
+# the data is oriented from south to north becasue of the radar flight direction
+
+##############################################################
+#################### lat lon height (llh) ####################
+##############################################################
+
+llh <-"/Volumes/JT/projects/uavsar/jemez/look_vector/BU/alamos_35915_03_BU_s1_2x8.llh"
+llh_hdr <-"/Volumes/JT/projects/uavsar/jemez/look_vector/BU/alamos_35915_03_BU_s1_2x8.llh.hdr"
+llh_envi <-read.ENVI(llh, llh_hdr) 
+readLines(llh_hdr)
+dim(llh_envi)
+
+# pull out first layer from data cube, latitude
+lat <-matrix(llh_envi[,,1], nrow = 8636)
+dim(lat)
+lat_r <-terra::rast(lat)
+plot(lat_r)
+
+# pull out second layer, longtiude
+lon <-matrix(llh_envi[,,2], nrow = 8636)
+dim(lon)
+lon_r <-terra::rast(lon)
+plot(lon_r)
+
+#third layer, height or elevation
+ele <-matrix(llh_envi[,,3], nrow = 8636)
+ele_r <-terra::rast(ele)
+plot(ele_r)
+
+########## saving single layers, or trying to
+
+# print llh header
+readLines(llh_hdr)
+
+# test this build vrt function
+setwd("/Users/jacktarricone/warp_test/good_llh_vrt/")
+ele_raster <-raster::raster(ele)
+raster::hdr(ele_raster, format="VRT", filename = "ele")
+plot(ele_raster)
+
+lat_raster <-raster::raster(lat)
+raster::hdr(lat_raster, format="VRT", filename = "lat")
+
+read_back_in <-"/Users/jacktarricone/warp_test/good_llh_vrt/ele.vrt"
+hmmt <-raster::raster(read_back_in)
+plot(hmmt)
 
 
-##### look vector
+# redefine data type to default on 4
+write.ENVI2 <- function(X, filename, interleave=c("bsq", "bil", "bip") ) 
+{ # write matrix or data cube to binary ENVI file
+  if (is.vector(X)) {
+    nCol = length(X)
+    nRow <- nBand <- 1
+  } else {
+    d = dim(X)
+    nRow  = d[1]
+    nCol  = d[2]
+    nBand = prod(d)/(nRow*nCol)
+  }
+  dim(X) = c(nRow, nCol, nBand)    # make it into 3D array in case it was not
+  
+  # check data type
+  data.type = 0
+  if (is.double (X)) data.type = 4 # 64-bit double
+  if (is.integer(X)) data.type = 3 # 32-bit int
+  if (is.complex(X)) data.type = 9 # 2x64-bit complex<double>
+  if (data.type == 0) {            # do not know what is it -> make it a double
+    X = as.double(X) 
+    data.type = 5 
+  } 
+  
+  # change interleave and store tha data
+  interleave = match.arg(interleave)
+  if      (interleave=="bil") X=aperm(X, c(2,3,1))  # R's [row,col,band] -> bil [col,band,row] 
+  else if (interleave=="bip") X=aperm(X, c(3,2,1))  # R's [row,col,band] -> bip [band,col,row] 
+  else if (interleave=="bsq") X=aperm(X, c(2,1,3))  # R's [row,col,band] -> bsq [col,row,band] 
+  writeBin(as.vector(X), filename)                  # write Envi file
+  
+  # write header file
+  out  = "ENVI\ndescription = { R-language data }\n"
+  out  = paste(out, "samples = ", nCol, "\n", sep="")
+  out  = paste(out, "lines = ", nRow, "\n", sep="")
+  out  = paste(out, "bands = ", nBand, "\n", sep="")
+  out  = paste(out, "data type = ",data.type,"\n", sep="")
+  out  = paste(out, "header offset = 0\n", sep="")
+  out  = paste(out, "interleave = ",interleave,"\n", sep="")   # interleave is assumed to be bsq - in case of 1 band images all 3 formats are the same 
+  ieee = if(.Platform$endian=="big") 1 else 0       # does this machine uses ieee (UNIX) format? or is it intel format?
+  out  = paste(out, "byte order = ", ieee, "\n", sep="")
+  cat(out, file=paste(filename, ".hdr", sep=""))
+  invisible(NULL)
+}
+
+write.ENVI2(lat, "/Users/jacktarricone/warp_test/good_llh_vrt/lat.llh", interleave = c("bsq"))
+write.ENVI2(lon, "/Users/jacktarricone/warp_test/good_llh_vrt/lon.llh", interleave = c("bsq"))
+write.ENVI2(ele, "/Users/jacktarricone/warp_test/good_llh_vrt/ele.llh", interleave = c("bsq"))
+
+
+# test loading data back in
+ele_path <-"/Users/jacktarricone/warp_test/good_llh_vrt/ele.llh"
+ele_hdr <-"/Users/jacktarricone/warp_test/good_llh_vrt/ele.llh.hdr"
+readLines(ele_hdr)
+
+ele_envi <-read.ENVI(ele, headerfile=paste(ele, ".hdr", sep="")) 
+readLines(ele_hdr)
+dim(ele_envi)
+q <-rast(ele_envi)
+plot(q)
+q
+
+
+
+
+###########################################################
+#################### look vector (lkv) ####################
+###########################################################
 
 lkv <-"/Volumes/JT/projects/uavsar/jemez/look_vector/BU/alamos_35915_03_BU_s1_2x8.lkv"
-lvk_hdr <-"/Volumes/JT/projects/uavsar/jemez/look_vector/BU/alamos_35915_03_BU_s1_2x8.lkv.hdr"
+lkv_hdr <-"/Volumes/JT/projects/uavsar/jemez/look_vector/BU/alamos_35915_03_BU_s1_2x8.lkv.hdr"
 
 lkv_envi <-read.ENVI(lkv, headerfile=paste(lkv, ".hdr", sep="")) 
 readLines(lkv_hdr)
 dim(lkv_envi)
 
-# create mat
+# create eat mat
 east <-matrix(lkv_envi[,,1], nrow = 8636)
-east<-east[nrow(east):1,ncol(east):1] #flip
+dim(east)
+east_r <-rast(east)
+plot(east_r)
 
-# create mat
+# create north mat
 north <-matrix(lkv_envi[,,2], nrow = 8636)
-north<-north[nrow(north):1,ncol(north):1] #flip
+dim(north)
+north_r <-rast(north)
+plot(north_r)
 
-# create mat
+# create up mat
 up <-matrix(lkv_envi[,,3], nrow = 8636)
-up<-up[nrow(up):1,ncol(up):1] #flp
 
-write.ENVI(east, "/Volumes/JT/projects/uavsar/jemez/look_vector/BU/envi_files/east.lkv", interleave = c("bsq"))
-write.ENVI(up, "/Volumes/JT/projects/uavsar/jemez/look_vector/BU/envi_files/up.lkv", interleave = c("bsq"))
-write.ENVI(north, "/Volumes/JT/projects/uavsar/jemez/look_vector/BU/envi_files/north.lkv", interleave = c("bsq"))
-
-##### lat lon dem
-
-llh <-"/Volumes/JT/projects/uavsar/jemez/look_vector/BU/alamos_35915_03_BU_s1_2x8.llh"
-
-llh_envi <-read.ENVI(llh, headerfile=paste(llh, ".hdr", sep="")) 
-readLines(llh_hdr)
-dim(llh_envi)
+write.ENVI2(east, "/Users/jacktarricone/warp_test/good_llh_vrt/east", interleave = c("bsq"))
+write.ENVI2(north, "/Users/jacktarricone/warp_test/good_llh_vrt/north", interleave = c("bsq"))
+write.ENVI2(up, "/Users/jacktarricone/warp_test/good_llh_vrt/up", interleave = c("bsq"))
 
 
-lat <-matrix(llh_envi[,,1], nrow = 8636)
-lat<-lat[nrow(lat):1,ncol(lat):1]
-
-lon <-matrix(llh_envi[,,2], nrow = 8636)
-lon<-lon[nrow(lon):1,ncol(lon):1]
-
-ele <-matrix(llh_envi[,,3], nrow = 8636)
-ele<-ele[nrow(ele):1,ncol(ele):1]
-
-write.ENVI(lat, "/Volumes/JT/projects/uavsar/jemez/look_vector/BU/envi_files/lat.llh", interleave = c("bsq"))
-write.ENVI(lon, "/Volumes/JT/projects/uavsar/jemez/look_vector/BU/envi_files/lon.llh", interleave = c("bsq"))
-write.ENVI(ele, "/Volumes/JT/projects/uavsar/jemez/look_vector/BU/envi_files/ele.llh", interleave = c("bsq"))
-
-#############################
-#### rotation of axes
-###########################
-
-#### lat lon
-# Corner 1 (&) = 35.734333038, -106.314704895
-# Corner 2 (&) = 35.718479156, -106.553138733       
-# Corner 3 (&) = 36.107688904, -106.334304810    
-# Corner 4 (&) = 36.091930389, -106.563705444  
-
-#### utm zone 13 north (easting, northing)
-# Corner 1 (&) = 381109, 3955279 
-# Corner 2 (&) = 359518, 3953836
-# Corner 3 (&) = 379903, 3996717
-# Corner 4 (&) = 359225, 3995277
 
 
-#nm
-nm_shape <-readOGR("/Volumes/JT/projects/uavsar/jemez/look_vector/nm_shape/tl_2017_35_tract.shp") 
 
-# create pts df
-pts<-as.matrix(rbind(c(381109, 3955279 ), c(359518, 3953836), c(379903, 3996717), c(359225, 3995277)))
-names(pts) <-c('x','y')
-corners <-st_multipoint(pts)
-plot(corners, main = "Corners")
-class(corners)
-
-#convert to corrdinates and define initial coordinates systems
-coordinates(corners) <- c('x', 'y')
-proj4string(corners)=CRS("+init=epsg:32613")  #32613 is the grid for new mexico
-plot(ele_resamp)
-plot(corners)
-
-#Transform the Ireland's grid to longitude & latitude
-coord<-spTransform(point,CRS("+init=epsg:4326"))
-coord
-
-?tm_shape
-tm_shape(nm_shape) +
-  tm_polygons() +
-  tm_graticules(col = "grey60") +
-  tm_shape(corners) +
-  tm_symbols(col = "black") +
-  tm_scale_bar(position = c("left", "bottom")) +
-  tm_shape(pts) +
-  tm_text("pt", ymod = -1)
