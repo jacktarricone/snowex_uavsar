ef242a76ab09720d42bef8e38583398e3bfc0d37
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2021-11-10 16:19:40 -0800

    creating lidar .inc angle, almost done

--- gradient_calc.R
+++ gradient_calc.R
@@ -1,4 +1,19 @@
 
+# november 10th, 2021
+# jack tarricone
+# almost done
+
+####
+# using the function cgrad, from the insol package
+# calculate the 3d (x,y,z) unit vector for a valle grande crop of the lidar dem
+# then add? not sur yet, to make surface normal vector
+
+library(terra)
+library(raster)
+library(rgdal)
+library(insol) # https://rdrr.io/cran/insol/man/cgrad.html
+
+
 # notes from meeting with HP October 25th
 
 ### creating new incidence angle raster from CZO LiDAR data
@@ -16,36 +31,35 @@
 # cos^-1((n1*n2+e1*e2+up1*up2)/(distance calc through atm for each vector))
 
 # packages in r or python for calculating gradients and surface normals
-library(terra)
-library(raster)
-library(rgdal)
-library(insol) # https://rdrr.io/cran/insol/man/cgrad.html
 
-# switched back to using the raster package vs cgrad can injest only rasters not SpatRasters!
 
-# bring in lidar dem
+
+#########################################################################
+
+  
+  
+# bring in lidar dem with raster not terra
+# switched back to using the raster package bc cgrad can injest only rasters not SpatRasters!
 lidar_dem <-raster("/Volumes/JT/projects/uavsar/jemez/lidar/ele_filt/GISdata/JemezRiver/lidar/snow_off/valles_elev_filt.img")
 plot(lidar_dem, col = terrain.colors(3000)) # test plot
 
 # crop down
-crop_ext <-extent(360000, 374000, 3965000, 3980000) # set vg UTM extent
+crop_ext <-extent(359000, 374000, 3965000, 3980000) # set vg UTM extent
 lidar_crop <-crop(lidar_dem, crop_ext)
 plot(lidar_crop, col = terrain.colors(3000)) # test, good
 
-# bring in resampled lidar
-# lidar_resamp <-raster("/Volumes/JT/projects/uavsar/jemez/lidar/lidar_uavsar_dem_resamp.tif")
-# plot(lidar_resamp, col = terrain.colors(3000)) # test, good
-# crop_ext <-extent(-106.55545, -106.40, 35.8, 35.95)
-# resamp_crop <-crop(lidar_resamp, crop_ext)
-#  
-# plot(resamp_crop, col = terrain.colors(3000)) # test plot
+# plv <-rast("/Volumes/JT/projects/uavsar/jemez/look_vector/plv_km_good.tif")
+# 
+# lidar_sp <-rast(lidar_crop)
+# test_resamp <-resample(lidar_sp, plv, method = "bilinear")
+# plot(plv)
+# plot(test_resamp, add = TRUE)
 
-# crop to there are no NaNs
-# crop_ext <-ext(-106.55545, -106.40, 35.8, 35.95)
-# resamp_crop <-crop(lidar_resamp, crop_ext)
-# plot(resamp_crop)
+########
+# calculate the gradient in all three dimensions
+# this function create a matrix for the x,y,z competent of a unit vector
+########
 
-# calculate the gradident in all three dimensions
 grad_mat <-cgrad(lidar_crop, 1, 1, cArea = FALSE)
 
 # make individual raster layer for each competent
@@ -67,8 +81,15 @@ z_comp <-raster(grad_mat[,,3], crs = crs(lidar_crop))
 extent(z_comp) <-extent(crop_ext)
 plot(z_comp)
 
-# cos^-1((n1*n2+e1*e2+up1*up2)/(distance calc through atm for each vector))
-surf_norm <-(x_comp*x_comp)+(y_comp*y_comp)+(z_comp*z_comp)
+# convert to spatrasts
+x_comp <-rast(x_comp)
+y_comp <-rast(y_comp)
+z_comp <-rast(z_comp)
+
+# calculate surface normal
+# i'm not sure this is right
+surf_norm <-(x_comp)+(y_comp)+(z_comp)
+surf_norm
 plot(surf_norm)
 
 ######### next steps
@@ -77,83 +98,38 @@ plot(surf_norm)
 # then do .inc angle calc
 # inc_angle <-cos^-1(-surf_norm*plk)
 
-
 # bring in path length vector data
 plv <-rast("/Volumes/JT/projects/uavsar/jemez/look_vector/plv_km_good.tif")
 plot(plv)
 
 # convert surface normal raster to spatrast for resampling
 # terra can go from utm to lat/lon, raster cannot!
-surf_norm_sp <-rast(surf_norm)
-
 # resample surf norm file up to plv file
-surf_norm_resamp5.6 <-resample(surf_norm_sp, plv, method = "bilinear")
+surf_norm_resamp5.6 <-resample(surf_norm, plv, method = "bilinear")
 
+# plot path length vector with resample surf_norm on top
 plot(plv)
 plot(surf_norm_resamp5.6, add = TRUE)
 
-inc_angle_rad <-(acos)(-surf_norm_resamp5.6*plv)
+# cos^-1((n1*n2+e1*e2+up1*up2)/(distance calc through atm for each vector))
+# calculate incidence angle
+inc_angle_rad <-(acos)(surf_norm_resamp5.6/(plv))
 inc_angle_deg <-inc_angle_rad*(180/pi)
 plot(inc_angle_deg)
-
-cos^-1((y_comp+x_comp+z_comp)/(distance calc through atm for each vector))
-
-
+plot(inc_angle_rad)
+writeRaster(inc_angle_rad,"/Volumes/JT/projects/uavsar/jemez/lidar_inc_angle.tif", overwrite = TRUE)
 
 
 
 
 
 
-# bring in resampled lidar
-# lidar_resamp <-raster("/Volumes/JT/projects/uavsar/jemez/lidar/lidar_uavsar_dem_resamp.tif")
-# plot(lidar_resamp, col = terrain.colors(3000)) # test, good
-# crop_ext <-extent(-106.55545, -106.40, 35.8, 35.95)
-# resamp_crop <-crop(lidar_resamp, crop_ext)
-#  
-# plot(resamp_crop, col = terrain.colors(3000)) # test plot
-
-# crop to there are no NaNs
-# crop_ext <-ext(-106.55545, -106.40, 35.8, 35.95)
-# resamp_crop <-crop(lidar_resamp, crop_ext)
-# plot(resamp_crop)
 
 
 
 
 
 
-# set random 1000x1000 cell crop extent
-crop_ext <-ext(360000, 361000, 3970000, 3971000)
-
-# crop to ext
-lidar_crop <-crop(lidar_dem, crop_ext)
-plot(lidar_crop,  col = terrain.colors(3000)) # test plot
-
-# covert crop into matrix, no NaNs, good
-dem_mat <-matrix(lidar_crop, nrow = 1000)
 
 
 
-
-
-inc_angle <-cos^-1(-surf_norm*plk)
-cos^-1((y_comp+x_comp+z_comp)/(distance calc through atm for each vector))
-
-
-##### from stack exchange
-# https://stackoverflow.com/questions/25704711/how-do-i-calculate-the-gradient-of-a-matrix-to-draw-a-vector-field-in-r
-
-bdiff <- function(x) c(NA,diff(x))
-x_grad <-t(apply(dem_mat,1,bdiff))
-y_grad <-apply(dem_mat,2,bdiff)
-
-
-rr <- row(dem_mat)
-cc <- col(dem_mat)
-dx <- t(apply(dem_mat,1,bdiff))
-dy <- apply(dem_mat,2,bdiff)
-sc <- 0.25
-off <- -0.5 ## I *think* this is right since we NA'd row=col=1
-plot(rr,cc,col="gray",pch=16)
-arrows(rr+off,cc+off,rr+off+sc*dx,cc+off+sc*dy,length=0.05)
