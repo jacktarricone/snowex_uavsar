62d2fad74574f1a72e24561d67ee2aa7c8c06c79
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2021-12-02 22:20:20 -0800

    gpr vs uavsar plot getting there

--- gpr_vs_insar.R
+++ gpr_vs_insar.R
@@ -18,14 +18,17 @@ plot(d_swe) # test plot
 
 # feb 12
 feb12_csv <-read.csv("/Volumes/JT/projects/uavsar/jemez/gpr/SnowEx_Jemez_Feb12_GPR_ver2.2.csv")
+feb12_csv <-mutate(feb12_csv, SWE_cm = SWE_mm*(1/10)) # add cm col
 feb12 <-vect(feb12_csv, geom = c("Long","Lat"), crs = crs(d_swe))
 
 # feb 20
 feb20_csv <-read.csv("/Volumes/JT/projects/uavsar/jemez/gpr/SnowEx_Jemez_Feb20_GPR_ver2.csv")
+feb20_csv <-mutate(feb20_csv, SWE_cm = SWE_mm*(1/10))
 feb20 <-vect(feb20_csv, geom = c("Long","Lat"), crs = crs(d_swe))
 
 # feb 26
 feb26_csv <-read.csv("/Volumes/JT/projects/uavsar/jemez/gpr/SnowEx_Jemez_Feb26_GPR_ver2.csv")
+feb26_csv <-mutate(feb26_csv, SWE_cm = SWE_mm*(1/10))
 feb26 <-vect(feb26_csv, geom = c("Long","Lat"), crs = crs(d_swe))
 
 
@@ -60,19 +63,19 @@ plot(feb26_gpr)
 points(feb26, col = "green", cex = .1)
 hist(feb26_gpr, breaks = 100)
 
-?as.points
 # create columns for cell numbers for each date
 # feb 12
 cell_numbers_feb12 <-as.integer(cellFromXY(feb12_gpr, cbind(feb12_csv$Long, feb12_csv$Lat)))
-uavsar_swe <-extract(feb12_gpr, cell_numbers_feb12, cells = TRUE, xy = TRUE)
-feb12_csv <-cbind(feb12_csv, cell_numbers_feb12, uavsar_swe)
+uavsar_swe12 <-extract(feb12_gpr, cell_numbers_feb12, cells = TRUE, xy = TRUE)
+feb12_csv <-cbind(feb12_csv, cell_numbers_feb12, uavsar_swe12)
+names(feb12_csv)[15]<- "uavsar_d_swe_cm"
 head(feb12_csv)
 #write.table(feb12_csv, "/Volumes/JT/2021_1_fall_UNR/agu/data/feb12_cells.csv")
 
 # change cell number to 1 - how many ever there or for plotting purposes
-feb12_cells <-as.integer(cells(feb12_gpr))
-cell_seq <-as.integer(seq(1,220,1))
-new_cell_numbers <-as.data.frame(cell_numbers_feb12)
+# feb12_cells <-as.integer(cells(feb12_gpr))
+# cell_seq <-as.integer(seq(1,220,1))
+# new_cell_numbers <-as.data.frame(cell_numbers_feb12)
 
 # for (i in 1:length(cell_seq)){
 #   new_cell_numbers[new_cell_numbers == feb12_cells[i]] <- cell_seq[i]
@@ -84,18 +87,61 @@ new_cell_numbers <-as.data.frame(cell_numbers_feb12)
 
 # feb 20
 cell_numbers_feb20 <-cellFromXY(feb20_gpr, cbind(feb20_csv$Long, feb20_csv$Lat))
-feb20_csv <-cbind(feb20_csv, cell_numbers_feb20)
+uavsar_swe20 <-extract(feb12_gpr, cell_numbers_feb20, cells = TRUE, xy = TRUE)
+feb20_csv <-cbind(feb20_csv, cell_numbers_feb20, uavsar_swe20)
+names(feb20_csv)[15]<- "uavsar_d_swe_cm"
 head(feb20_csv)
 #write.table(feb20_csv, "/Volumes/JT/2021_1_fall_UNR/agu/data/feb20_cells.csv")
 feb20_cells <-cells(feb20_gpr)
 
 # feb 26
 cell_numbers_feb26 <-cellFromXY(feb26_gpr, cbind(feb26_csv$Long, feb26_csv$Lat))
-feb26_csv <-cbind(feb26_csv, cell_numbers_feb26)
+uavsar_swe26 <-extract(feb12_gpr, cell_numbers_feb26, cells = TRUE, xy = TRUE)
+feb26_csv <-cbind(feb26_csv, cell_numbers_feb26, uavsar_swe26)
+names(feb26_csv)[15]<- "uavsar_d_swe_cm"
 head(feb26_csv)
 #write.table(feb26_csv, "/Volumes/JT/2021_1_fall_UNR/agu/data/feb26_cells.csv")
 feb26_cells <-cells(feb26_gpr)
 
+##########################################################################
+###### create new data frames for plotting UAVSAR and GPR together #######
+##########################################################################
+
+# summarize from feb 12 all the gpr data by cell number
+head(feb12_csv)
+feb12_plotting <-feb12_csv%>%
+              group_by(cell_numbers_feb12)%>% 
+              summarise(feb12_gpr_median_swe=median(SWE_cm, na.rm = TRUE),
+                   feb12_gpr_QR1 = quantile(SWE_cm, c(0.25), na.rm = TRUE),
+                   feb12_gpr_QR3 = quantile(SWE_cm, c(0.75), na.rm = TRUE),
+                   feb12_gpr_mean_swe = mean(SWE_cm), 
+                   feb12_gpr_max_swe = max(SWE_cm), 
+                   feb12_gpr_min_swe = min(SWE_cm),
+                   feb12_gpr_std_swe = sd(SWE_cm), 
+                   uavsar_d_swe_cm = max(uavsar_d_swe_cm))
+# write.csv(feb12_plotting, "/Volumes/JT/2021_1_fall_UNR/agu/data/feb12_plotting.csv", row.names = FALSE)
+
+
+# summarize from feb 20 all the gpr data by cell number
+feb20_plotting <-feb20_csv%>%
+  group_by(cell_numbers_feb20)%>% 
+  summarise(feb20_gpr_median_swe=median(SWE_cm, na.rm = TRUE),
+            feb20_gpr_QR1 = quantile(SWE_cm, c(0.25), na.rm = TRUE),
+            feb20_gpr_QR3 = quantile(SWE_cm, c(0.75), na.rm = TRUE),
+            feb20_gpr_mean_swe = mean(SWE_cm), 
+            feb20_gpr_max_swe = max(SWE_cm), 
+            feb20_gpr_min_swe = min(SWE_cm),
+            feb20_gpr_std_swe = sd(SWE_cm))
+# write.csv(feb20_plotting, "/Volumes/JT/2021_1_fall_UNR/agu/data/feb20_plotting.csv", row.names = FALSE)
+
+# filter for only the cell numbers that apear in both data frames
+feb12_same_cells <-filter(feb12_plotting, cell_numbers_feb12 %in% feb20_plotting$cell_numbers_feb20)
+feb20_same_cells <-filter(feb20_plotting, cell_numbers_feb20 %in% feb12_plotting$cell_numbers_feb12)
+
+# stitch back together and save
+first_flight_gpr_cells <-cbind(feb12_same_cells, feb20_same_cells)
+head(first_flight_gpr_cells)
+# write.csv(first_flight_gpr_cells, "/Volumes/JT/2021_1_fall_UNR/agu/data/first_flight_gpr_cells.csv", row.names = FALSE)
 
 ##############################################
 ###### testing plotting by cell number #######
@@ -105,23 +151,23 @@ mini_feb12 <-filter(feb12_csv, cell_numbers_feb12 == 2096)
 mini_feb20 <-filter(feb20_csv, cell_numbers_feb20 == 2096)
 mini_feb26 <-filter(feb26_csv, cell_numbers_feb26 == 2096)
 
-ggplot() + 
-  geom_pointrange(feb12_csv, mapping = aes(x = cell_numbers_feb12, y = SWE_mm, group = cell_numbers_feb12),
+theme_set(theme_light(18))
+p2 <-ggplot() + 
+  geom_pointrange(feb12_csv, mapping = aes(x = cell_numbers_feb12, y = SWE_cm, group = cell_numbers_feb12),
                   stat = "summary",
                   fun.min = function(z) { quantile(z,0.25) },
                   fun.max = function(z) { quantile(z,0.75) },
                   fun = median,
                   size = .2,
                   color = "firebrick")+
-  geom_pointrange(feb20_csv, mapping = aes(x = cell_numbers_feb20, y = SWE_mm, group = cell_numbers_feb20),
+  geom_pointrange(feb20_csv, mapping = aes(x = cell_numbers_feb20, y = SWE_cm, group = cell_numbers_feb20),
                   stat = "summary",
                   fun.min = function(z) { quantile(z,0.25) },
                   fun.max = function(z) { quantile(z,0.75) },
                   fun = median,
                   size = .2,
                   color = "black")+
-  geom_point(feb12_csv, mapping = aes(x = cell_numbers_feb12, y = lyr.1, group = cell_numbers_feb12)) 
-  geom_pointrange(mini_feb26, mapping = aes(x = cell_numbers_feb26, y = SWE_mm, group = cell_numbers_feb26),
+  geom_pointrange(feb26_csv, mapping = aes(x = cell_numbers_feb26, y = SWE_cm, group = cell_numbers_feb26),
                   stat = "summary",
                   fun.min = function(z) { quantile(z,0.25) },
                   fun.max = function(z) { quantile(z,0.75) },
@@ -129,9 +175,9 @@ ggplot() +
                   size = .2,
                   color = "green")
 
-ggsave(p1,
-       file = "jemez_pit_density2.png",
-       width = 7, 
+ggsave(p2,
+       file = "/Volumes/JT/2021_1_fall_UNR/agu/data/gpr_swe_v2.png",
+       width = 11, 
        height = 4,
        dpi = 400)
 
@@ -141,21 +187,6 @@ ggsave(p1,
 
 
 
-swe_matix <-matrix(swe_crop, nrow = 62)
-all_cells_not_na <-cells(swe_crop)
 
-plot(swe_crop)
-ncell(swe_crop)
-nsrc(swe_crop)
-nrow(swe_crop)
-ncol(swe_crop)
-
-
-
-?extract
-test <-extract(gpr_only_pixels, 
-               feb12)
-
-test <-as.data.frame(cellFromXY(gpr_only_pixels, cbind(feb12_csv$Long,feb12_csv$Lat)))
 
 
