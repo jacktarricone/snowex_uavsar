92fc3e6eaa3ce05f63a69e1875cfd94b5c44f2f1
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2022-02-04 14:55:05 -0800

    updating fsca scripts

--- jemez_fsca.R
+++ jemez_fsca.R
@@ -8,9 +8,9 @@ dem <-rast("/Users/jacktarricone/ch1_jemez_data/gpr_rasters_ryan/dem_feb12-19.ti
 dem
 
 # bringg in raw fSCA data downloaded from landsat web portal
-fsca_raw <-rast("/Users/jacktarricone/ch1_jemez_data/landsat_fsca/LC08_CU_010012_20200218_20200227_C01_V01_SNOW/LC08_CU_010012_20200218_20200227_C01_V01_SNOW.tif")
+fsca_raw <-rast("/Users/jacktarricone/ch1_jemez_data/landsat_fsca/feb_18/LC08_CU_010012_20200218_20200227_C01_V01_SNOW.tif")
 fsca_raw # check
-values(fsca_raw)[values(fsca_raw) == 0] = NA # 0 to NaN
+# values(fsca_raw)[values(fsca_raw) == 0] = NA # 0 to NaN
 fsca_raw <-fsca_raw/10 # correct [%] scale
 plot(fsca_raw) # test plot
 
@@ -22,44 +22,8 @@ fsca <-mask(fsca_crop, dem, maskvalue = NA)
 plot(dem)
 plot(fsca, add=TRUE)
 
-#save
-# writeRaster(fsca, "/Users/jacktarricone/ch1_jemez_data/gpr_rasters_ryan/landsat_fsca_2-18.tif")
-
-
-
-
-
-
-
-
-
-
-# old code
-
-# mask for cor values and save it
-fsca_final <-mask(fsca_crop_resamp, cor, maskvalue = NA)
-plot(cor)
-plot(fsca_final, add=TRUE)
-writeRaster(fsca_final, "/Volumes/JT/projects/uavsar/jemez/atm_correct/fsca.tif")
-
-#create snow mask for pixels that are over 90 percent snow
-snow_mask <-fsca_final
-values(snow_mask)[values(snow_mask) > 0] = 1
-plot(snow_mask)
-#writeRaster(snow_mask, "/Volumes/JT/projects/uavsar/jemez/atm_correct/snow_mask.tif")
-
-#create no snow mask for pixels that are over 90 percent snow
-no_snow_mask <-fsca_final
-values(no_snow_mask)[values(is.na(no_snow_mask))] = -999
-values(no_snow_mask)[values(no_snow_mask) > 0] = NA
-values(no_snow_mask)[values(no_snow_mask) < 0] = 1
-no_snow_mask <-mask(no_snow_mask, cor, maskvalue = NA)
-plot(no_snow_mask)
-hist(no_snow_mask)
-plot(snow_mask, add = TRUE)
-#writeRaster(no_snow_mask, "/Volumes/JT/projects/uavsar/jemez/atm_correct/no_snow_mask.tif")
-
-
+# save
+# writeRaster(fsca, "/Users/jacktarricone/ch1_jemez_data/gpr_rasters_ryan/landsat_fsca_2-18v2.tif")
 
 
 
