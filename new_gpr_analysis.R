e52637b0f6ac697a0a08d0fcbcfa7dd59b1ea7c0
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2022-02-10 11:22:13 -0800

    add scatters

--- new_gpr_analysis.R
+++ new_gpr_analysis.R
@@ -66,18 +66,88 @@ unw2_final <-mask(unw2_resamp, unw1_final, maskvalues = NA)
 plot(unw1_final) # test
 plot(unw2_final, add = TRUE)
 
-# cumulative phase
-unw_cum <-unw1_final+unw2_final
+# cmulative phase
+unw_cm <-unw1_final+unw2_final
 
 # save rasters
 # writeRaster(unw1_final, "/Users/jacktarricone/ch1_jemez_data/gpr_rasters_ryan/unw1_final.tif")
 # writeRaster(unw2_final, "/Users/jacktarricone/ch1_jemez_data/gpr_rasters_ryan/unw2_final.tif")
-# writeRaster(unw_cum, "/Users/jacktarricone/ch1_jemez_data/gpr_rasters_ryan/unw_cum.tif")
+# writeRaster(unw_cm, "/Users/jacktarricone/ch1_jemez_data/gpr_rasters_ryan/unw_cm.tif")
 
 
 #######
 #######
 ## bring in added phase raster aka 2/12-2/26
-# unw_cum <-rast("/Users/jacktarricone/ch1_jemez_data/gpr_rasters_ryan/unw_cum.tif")
+# unw_cm <-rast("/Users/jacktarricone/ch1_jemez_data/gpr_rasters_ryan/unw_cm.tif")
 
-# bring in 2/12-2/26 gpr rdata
+# bring in 2/12-2/26 gpr data
+feb26_minus_feb12 <-rast("/Users/jacktarricone/ch1_jemez_data/gpr_rasters_ryan/Rasters_for_Jack/feb26_minus_feb121.tif")
+plot(feb26_minus_feb12)
+
+# resample gpr to same grid as unw, crop ext
+unw_cm_crop <-crop(unw_cm, ext(feb26_minus_feb12)) # crop
+feb26_minus_feb12 
+unw_cm_crop # check
+
+# test plot
+plot(unw_cm_crop)
+plot(feb26_minus_feb12, add = TRUE, col = "red")
+
+# mask unw data with gpr
+unw_cm_crop_mask <-mask(unw_cm_crop, feb26_minus_feb12, maskvalue = NA)
+f26_m_12_mask <-mask(feb26_minus_feb12, unw_cm_crop_mask, maskvalue = NA)
+
+# plot only pixels that have data for both gpr and unw
+plot(unw_cm_crop_mask)
+plot(f26_m_12_mask, add = TRUE, col = hcl.colors(12, "Berlin"))
+
+# convert raster to dataframe
+unw_df_cm <-as.data.frame(unw_cm_crop_mask, xy = TRUE, cells = TRUE, na.rm = TRUE)
+gpr_df_cm <-as.data.frame(f26_m_12_mask, xy = TRUE, cells = TRUE, na.rm = TRUE)
+head(unw_df_cm)
+head(gpr_df_cm)
+tail(unw_df_cm)
+tail(gpr_df_cm)
+
+
+# bind the data frames
+cm_plotting_df <-cbind(unw_df_cm, gpr_df_cm$feb26_minus_feb121)
+head(cm_plotting_df)
+colnames(cm_plotting_df)[4] <- "unw" # rename col 4
+colnames(cm_plotting_df)[5] <- "gpr_twt_change_ns" # rename col 5
+head(cm_plotting_df)
+
+# test plot
+theme_set(theme_light(11)) # set theme
+
+ggplot(cm_plotting_df) +
+  geom_point(aes(y = unw, x = gpr_twt_change_ns))
+
+# add lm
+cm_lm <-lm(unw ~ gpr_twt_change_ns, cm_plotting_df)
+summary(cm_lm)
+
+ggplot(cm_plotting_df, aes(y = unw, x = gpr_twt_change_ns)) +
+  geom_point()+
+  geom_abline(slope = coef(cm_lm)[["gpr_twt_change_ns"]], 
+              intercept = coef(cm_lm)[["(Intercept)"]])
+
+#### testing putting things on same scale
+cm_test <-cm_plotting_df
+
+# check min
+cm_test$unw_norm <-cm_test$unw-min(cm_test$unw)
+cm_test$gpr_norm <-cm_test$gpr_twt_change_ns-min(cm_test$gpr_twt_change_ns)
+head(cm_test)
+
+ggplot(cm_test) +
+  geom_point(aes(y = unw_norm, x = gpr_norm))
+
+# add lm
+cm_lm_norm <-lm(unw_norm ~ gpr_norm, cm_test)
+summary(cm_lm_norm)
+
+ggplot(cm_test, aes(y = unw_norm, x = gpr_norm)) +
+  geom_point()+
+  geom_abline(slope = coef(cm_lm_norm)[["gpr_norm"]], 
+              intercept = coef(cm_lm_norm)[["(Intercept)"]])
