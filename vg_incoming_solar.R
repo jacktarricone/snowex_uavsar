c25f2ff8db02fb02df27d50b6da06b926ce54186
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2022-04-07 16:51:11 -0700

    finally

--- vg_incoming_solar.R
+++ vg_incoming_solar.R
@@ -33,6 +33,8 @@ vg_dem <-rast("/Users/jacktarricone/ch1_jemez_data/jemez_lidar/vg_dem_latlon_5.6
 dem_raster <-raster(vg_dem) 
 grad_mat <-cgrad(dem_raster, cArea = FALSE) # compute normal vector for dem a 1m cell cize
 
+total_daily_solar <-function(day_of_study){
+  
 ## define variables for insolation function
 height <-2600 # height asl [m]
 visibility <-90 # [km]
@@ -43,7 +45,7 @@ tempK <- 278 # temp in kelvin
 tmz <- -7 # time zone
 year <- 2020
 month <- 2
-day <- 26
+day <- day_of_study
 timeh <- 0
 jd <- JDymd(year,month,day,hour=timeh)
 
@@ -68,7 +70,7 @@ Iglobal_v2 <-array(numeric(),c(nrow,ncol,nlay))
 for (i in 1:nlay){
   hours <-seq(day_one[1],day_one[2],deltat)
   layers <-seq(1,12,1)
-  jd = JDymd(year,month,day,hour=hours[1])
+  jd = JDymd(year,month,day,hour=hours[i])
   sv = sunvector(jd,lat,lon,tmz)
   hsh = as.array(hillshading(grad_mat,sv),dim=dim(dem_raster))
   sh = doshade(dem_raster,sv)
@@ -76,40 +78,37 @@ for (i in 1:nlay){
   Idirdif = insolation(zenith,jd,height,visibility,RH,tempK,0.03,0.60)
   ## direct radiation modified by terrain + diffuse irradiation (skyviewfactor ignored)
   ## values in J/m^2
-  Iglobal_v2[,,layers[1]] <- Iglobal[,,] + (Idirdif[1,1] * hsh + Idirdif[1,2])*3600*deltat
+  Iglobal_v2[,,layers[i]] <- Iglobal[,,] + (Idirdif[1,1] * hsh + Idirdif[1,2])*3600*deltat
 }
 
-
-library(terra)
 solar_rad_hourly_j <-rast(Iglobal_v2)
 crs(solar_rad_hourly_j) <-crs(vg_dem)
 ext(solar_rad_hourly_j) <-ext(vg_dem)
-plot(solar_rad_hourly_j)
+# plot(solar_rad_hourly_j)
 
-solar_rad_kwh <-solar_rad_hourly_j/3.6e6
-plot(solar_rad_kwh)
+solar_rad_total_j <-app(solar_rad_hourly_j, sum)
+# plot(solar_rad_total_j)
 
-# writeRaster(solar_rad_MJ, "/Users/jacktarricone/ch1_jemez_data/jemez_lidar/solar_rad_MJ.tif")
+solar_rad_total_kwh <-solar_rad_total_j/3.6e6
+# plot(solar_rad_total_kwh)
+solar_rad_total_MJ <-solar_rad_total_j/1e6
+# plot(solar_rad_total_MJ)
+return(solar_rad_total_kwh)
+}
 
-Iglobal = raster(Iglobal_v2,crs=projection(vg_dem))
-extent(Iglobal) = extent(raster(vg_dem))
-plot(Iglobal*1e-6,col=grey(1:100/100),
-     legend.args = list(text=expression(paste('Insolation MJ ',m^-2)), side=4,line=2.5))
-contour(vg_dem,lwd = 0.5,col='sienna1',add=TRUE,levels=seq(0,2500,500))
-contour(vg_dem,lwd = 0.25,col='sienna1',add=TRUE,levels=seq(0,2500,50),drawlabels=FALSE)
-unlink(demfile)
+days_list <-as.list(seq(12,26,1))
+solar_list <-lapply(days_list, total_daily_solar)
+plot(solar_list[[15]])
+plot(solar_list[[1]])
 
-# 1 Kw·h = 1 kW x 3600 s = 3.6 MJ = 3.6*1e6 J
-# conver kilowatt-hours to megajoules
+solar_stack <-rast(solar_list)
+solar_stack
 
-total_kwh_feb26 <-5.51
-total_MJ_feb26 <-total_kwh_feb26*3.6
+avg_solar_kwh_feb12_26 <-app(solar_stack, mean)
+plot(avg_solar_kwh_feb12_26)
 
-J_feb12 <-3.65*1e6
+writeRaster(avg_solar_kwh_feb12_26, "/Users/jacktarricone/ch1_jemez_data/jemez_lidar/avg_solar_kwh_feb12_26.tif")
 
-demfile = tempfile()
-download.file("https://meteoexploration.com/R/insol/data/demlapalma.tif",demfile)
-dem = raster(demfile)
-plot(dem)
+
 
 
